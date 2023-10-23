e383d1ee918d4a1db53e271634b1ee9ff196a2e3
Emory <ro25ck@yahoo.com>
2023-10-23 15:20:15 +0100

    route setup, article section data render setup installed

---
+++ src/components/Articles.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+
+const data = [
+    {
+      title: "Living in the shadow of a great man",
+      topic: "mitch",
+      author: "butter_bridge",
+      body: "I find this existence challenging",
+      created_at: 1594329060000,
+      votes: 100,
+      article_img_url:
+        "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
+    },
+    {
+      title: "Sony Vaio; or, The Laptop",
+      topic: "mitch",
+      author: "icellusedkars",
+      body: "Call me Mitchell. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would buy a laptop about a little and see the codey part of the world. It is a way I have of driving off the spleen and regulating the circulation. Whenever I find myself growing grim about the mouth; whenever it is a damp, drizzly November in my soul; whenever I find myself involuntarily pausing before coffin warehouses, and bringing up the rear of every funeral I meet; and especially whenever my hypos get such an upper hand of me, that it requires a strong moral principle to prevent me from deliberately stepping into the street, and methodically knocking people’s hats off—then, I account it high time to get to coding as soon as I can. This is my substitute for pistol and ball. With a philosophical flourish Cato throws himself upon his sword; I quietly take to the laptop. There is nothing surprising in this. If they but knew it, almost all men in their degree, some time or other, cherish very nearly the same feelings towards the the Vaio with me.",
+      created_at: 1602828180000,
+      article_img_url:
+        "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
+    },
+    {
+      title: "Eight pug gifs that remind me of mitch",
+      topic: "mitch",
+      author: "icellusedkars",
+      body: "some gifs",
+      created_at: 1604394720000,
+      article_img_url:
+        "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
+    }
+]
+
+    const articleListInfo = data.map((article)=> {
+        const {title, author, created_at, topic, body, article_img_url} = article
+        return (
+            <>
+            <li key={title}>{title}</li>
+            <li>{author}</li>
+            <li>{created_at}</li>
+            <li>{topic}</li>
+            <li>{body}</li>
+            <li><img src={article_img_url} alt={title}></img></li>
+            </>
+        )
+    })
+
+
+function Articles() {
+  return (<>
+    <section className="main-articleList">
+        <p>Here is the list all the articles on the site</p>
+        <section className="main-articleListInfo">
+            <ul>{articleListInfo}</ul>
+        </section>
+    </section>
+    </>
+  )
+}
+
+export default Articles
