8ab7db83380c825fad4dfc705102eb74216aae7c
Emory <ro25ck@yahoo.com>
2023-10-24 17:04:28 +0100

    cleaned up code for PR for ticket

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useState, useEffect } from 'react'
 import { getArticles } from '../api'
 
+
 function Articles() {
     const [isLoading, setIsLoading] = useState(true)
     const [data, setData] = useState([])
@@ -21,9 +22,9 @@ function Articles() {
 
     const articleListInfo = data.map((article)=> {
         const {article_id, title, author, created_at, topic, article_img_url, votes, comment_count} = article
-
+        const hrefArticle = "/articles/" + article_id
+        const hrefArticleComments = hrefArticle + "#article_comments"
 /* next iteration of page 
-   Image, Title, Comments links will navigate to SingleAritcle component with article id in url
    Category link will re-render current Articles component with filtered list of articles in queried category
    Author link will re-render current Articles component with filtered list of articles in queried author
 
@@ -32,15 +33,15 @@ function Articles() {
         return (
             <section className="articleCard" key={article_id}>
             <div>
-            <a href='#'><img src={article_img_url} alt={title} ></img></a>
+            <a href={hrefArticle}><img src={article_img_url} alt={title} ></img></a>
             </div>
             <div>
-            <p>Title: <a href="#">{title}</a></p>
+            <p>Title: <a href={hrefArticle}>{title}</a></p>
             <p>Author: <a href="#">{author}</a></p>
             <p>Category: <a href="#">{topic}</a></p>
             <p>Posted: {created_at.substring(0,10)}</p>
             <p>Votes: {votes}</p>
-            <p>Comment: <a href='#'>{comment_count}</a></p>
+            <p>Comment: <a href={hrefArticleComments}>{comment_count}</a></p>
             </div>
             </section>
         )
--- src/components/SingleArticle.jsx
+++ src/components/SingleArticle.jsx
@@ -3,18 +3,6 @@ import { useState, useEffect } from 'react'
 import { getArticleById } from '../api'
 import { useParams } from 'react-router'
 
-// const data = { 
-//     article_id: 2,
-//     title: "The Rise Of Thinking Machines: How IBM's Watson Takes On The World",
-//     topic: "coding",
-//     author: "jesshelly",
-//     body: "Many people know Watson as the IBM-developed cognitive super computer that won the Jeopardy! gameshow in 2011. In truth, Watson is not actually a computer but a set of algorithms and APIs, and since winning TV fame (and a $1 million prize) IBM has put it to use tackling tough problems in every industry from healthcare to finance. Most recently, IBM has announced several new partnerships which aim to take things even further, and put its cognitive capabilities to use solving a whole new range of problems around the world.",
-//     created_at: "2020-05-14T01:02:00.000Z",
-//     votes: 0,
-//     article_img_url:
-//       "https://images.pexels.com/photos/373543/pexels-photo-373543.jpeg?w=700&h=700"
-//   }
-
 const data2 = [{"comment_id":162,"body":"Et suscipit maxime sit sunt consequuntur consequatur fugiat molestias. Et quis enim vero.","article_id":2,"author":"grumpy19","votes":14,"created_at":"2020-10-03T19:22:00.000Z"},{"comment_id":102,"body":"Quia quos adipisci sint expedita voluptatem id ut at accusantium. Et ex itaque recusandae aut quo. Quia quam similique eum quidem iusto. Aspernatur ducimus vitae vel natus doloribus qui id. Excepturi voluptatem qui quia sit qui eveniet voluptatem. Fugit itaque libero quibusdam sunt.","article_id":2,"author":"jessjelly","votes":10,"created_at":"2020-09-14T12:13:00.000Z"}]
 
 const commentFormat = data2.map((comment) => {
@@ -63,7 +51,8 @@ export default function SingleArticle() {
     <div id="ArticleBody" className='SingleArticleBody'>
         <p>{data.body}</p>
     </div>
-    <div className='SingleArticleComments'>
+    <div id="ArticleComments" className='SingleArticleComments'>
+        <a id="article_comments"></a>
         <h4>Comments: {data2.length}</h4> 
         <p>{commentFormat}</p>
         <button >add comment</button>
