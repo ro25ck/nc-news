f38cab78830864e1fd5dc650170320e54ce53511
Emory <ro25ck@yahoo.com>
2023-10-26 15:21:58 +0100

    updated updateVotes function with correct endpoint spelling

---
+++ src/components/CommentInput.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function CommentInput() {
+  return (
+    <div>This will be a input box with button to submit and button to exit</div>
+  )
+}
+
+export default  CommentInput
