4c2d04f0e6bbe17fc8be79a3b46b4882291aa566
Emory <ro25ck@yahoo.com>
2023-10-26 13:48:00 +0100

    Add button functionality to change the displayed votes on SingleAritcle component

--- src/components/Votes.jsx
+++ src/components/Votes.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import { useState } from 'react'
 
 function Votes({data}) {
+    const [votesChange, setVotesChange] = useState(0)
+
+    const updateVoteDisplay = (value) =>{
+        setVotesChange((currentVotes)=> {
+            return currentVotes + value
+        })
+    }
 
   return (<>
-    Votes: {data.votes}
+    Votes: {data.votes + votesChange}
+    <button onClick={()=>{
+        updateVoteDisplay(1)
+        }}
+    >
+        +
+    </button>
+    <button onClick={()=>{
+        updateVoteDisplay(-1)
+        }}
+    >
+        -
+    </button>
     </>
   )
 }
