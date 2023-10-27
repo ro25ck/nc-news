e58ee462360c18aedbdc8dee4add8a674976f6b3
Emory <ro25ck@yahoo.com>
2023-10-27 10:09:32 +0100

    implemented input box for new comment

--- src/components/CommentInput.jsx
+++ src/components/CommentInput.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
+import {useState} from 'react'
 
 export default function CommentInput() {
-  return (<>
-    <div>this area needs to capture: username, comment body, timestamp</div>
-    <div id='form-new-comment'>
-        <form
-            label="user"
-            type="text"
-            placeholder="your username here..."
-        />
-    </div>
-    </>
+    const [message, setMessage] = useState("")
+  return (
+    <form>
+        <div id='form-new-comment'>
+            <textarea 
+                value={message}
+                onChange={e => setMessage(e.target.value)}
+            />
+        </div>
+        <div>
+            Error
+        </div>
+    </form>
   )
 }
